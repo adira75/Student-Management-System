056902add206dd686bbfb70635c3f69ac51d4daf
Aditi Ranjan <aditiranjan75@gmail.com>
2026-07-26 18:26:46 +0530

    Created student structure

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include<iostream>
 using namespace std;
 
+struct Student
+{
+    int rollNo;
+    string name;
+    int age;
+    string branch;
+    float marks;
+};
+
 int main()
 {
     cout<<"Student Management System";
+
     return 0;
 }
